ef4e46501b930f779f1602597917bdda1e6127b9
samuelezraberry <samuelezraberry@gmail.com>
2021-01-04 20:38:53 +0000

    Added dog_breeds

---
+++ python/dog_breeds.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url="https://dogtime.com/dog-breeds/profiles"
+
+response=requests.get(url)
+soup=BeautifulSoup(response.text, 'html.parser')
+
+breeds=soup.find_all("a",{"class":"list-item-title"})
+
+with open("dog_breeds.csv", 'w', newline="") as csvfile:
+    wr = csv.writer(csvfile)
+    for breed in breeds:
+        wr.writerow([breed.text])
+
